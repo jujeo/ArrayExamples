9f4028c6f2d2a3e118befe258590dd871401a4e7
jujeo <jujeo100@gmail.com>
2018-10-09 11:16:58 -0700

    Task1 & Task2

--- Main.java
+++ Main.java
@@ -30,14 +30,26 @@ class Main
          *          call this array ar1.  Print it out below.
          */
         System.out.println("*** PRINTING ar1 *");
+        int[] ar1 = new int[n];
         
+        for (int i=0; i<n; i++)
+            ar1[i]=i;
         
+        for (int i=0; i<n; i++)
+            System.out.println("ar1[" +i+ "] = "+ar1[i]);
                 
         /*
          * Task 2.  Create a new array called ar2.
          *          Copy the elements of ar1 into ar2.
          */
         System.out.println("*** PRINTING ar2 *");
+        int[] ar2 = new int[n];
+        
+        for (int i=0; i<n; i++)
+            ar2[i]=i;
+        
+        for (int i=0; i<n; i++)
+            System.out.println("ar2[" +i+ "] = "+ar2[i]);
         
         
         /* 
